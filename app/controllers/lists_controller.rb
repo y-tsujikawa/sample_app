9834774394b674e2bb7375595581e4a76535ea3b
y-tsujikawa <184855547+y-tsujikawa@users.noreply.github.com>
2024-10-25 11:11:30 +0000

    [Finish] 3-フラッシュメッセージを実装してみよう

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,9 +8,11 @@ class ListsController < ApplicationController
     @list = List.new(list_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
     if @list.save
-    # 4. トップ画面へリダイレクト
-      redirect_to list_path(list.id)
+      # 3. フラッシュメッセージを定義し、詳細画面へリダイレクト
+      flash[:notice] = "投稿に成功しました。"
+      redirect_to list_path(@list.id)
     else
+      flash.now[:alert] = "投稿に失敗しました。"
       render :new
     end
   end
